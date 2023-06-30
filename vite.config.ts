400a7dbdc34b24ff216cda9edfb2d5bb442032e1
Aleksey Likhodievski <aleksey.likhodievski@effective-soft.com>
2023-06-30 19:36:41 +0300

    chore: changed redirects

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default ({ mode }) => {
 				'/api': {
 					target: process.env.VITE_JOBS_API_URL,
 					changeOrigin: true,
-					secure: true,
+					secure: false,
 					rewrite: (path) => path.replace(/^\/api/, ''),
 				},
 			},
