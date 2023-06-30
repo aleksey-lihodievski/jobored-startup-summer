5f2cccef8cf39b07636d05cc979e7c0834ab816e
Aleksey Likhodievski <aleksey.likhodievski@effective-soft.com>
2023-06-30 18:11:02 +0300

    fix: probably solved production error

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default ({ mode }) => {
 				'/api': {
 					target: process.env.VITE_JOBS_API_URL,
 					changeOrigin: true,
+					secure: false,
 					rewrite: (path) => path.replace(/^\/api/, ''),
 				},
 			},
